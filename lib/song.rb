37cd7e8974fa80e2478ff542b3a3b603187f4d0a
Alyssa Yates <bosslys@icloud.com>
2019-09-18 21:10:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,11 +43,11 @@ def self.artist_count
     if artist_count[name]
       artist_count[name] += 1
     else
-      genre_count[style] = 1
+      artist_count[style] = 1
     end
   end
   return artist_count
-end 
+end
 
   # genre = "rap"
   # genre_count[rap] = 1
