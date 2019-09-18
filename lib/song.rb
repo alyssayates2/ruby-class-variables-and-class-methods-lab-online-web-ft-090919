12df8f926e9547ef57c8bb626310ac3d4e5226b7
Alyssa Yates <bosslys@icloud.com>
2019-09-18 20:33:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,13 @@
 class Song
-  def initialize(name, artist, genre)
-  end
+
   attr_accessor :name, :artist, :genre
+  
+  def initialize(name, artist, genre)
+    @name = name 
+    @artist = artist 
+    @genre = genre 
+  end
+
 
   @@count = 0
   def self.count
